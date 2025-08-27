51faab68497b748ac1200b8eb15f95459e701e85
profcintiapinho <profcintiapinho@gmail.com>
2025-08-27 11:58:00 -0300

    fazendo alguns para embeddings ler csv

--- setup_database.py
+++ setup_database.py
@@ -1,9 +1,11 @@
 import os
 from langchain_chroma import Chroma
-from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, CSVLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 import shutil
+import csv
+from langchain.schema import Document
 
 # Pasta dos documentos estão localizados 
 DOC_DIR = "documents"
@@ -18,12 +20,22 @@ if os.path.exists(DB_DIR):
 documents = []
 for file_name in os.listdir(DOC_DIR):
     file_path = os.path.join(DOC_DIR, file_name)
+    
     if file_name.endswith(".pdf"):
         loader = PyPDFLoader(file_path)
         documents.extend(loader.load())
-    else:
+    elif file_name.endswith(".txt"):
         loader = TextLoader(file_path, encoding="utf-8")
         documents.extend(loader.load())
+    elif file_name.endswith(".csv"):
+        with open(file_path, encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                text = row["Problema"]
+                metadata = {"URL": row.get("URL", "")}
+                documents.append(Document(page_content=text, metadata=metadata))
+    else:
+        print(f"Atenção: arquivo ignorado (não suportado): {file_name}")
 
 # Divide os documentos em chunks
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
